1d1fa444057ac9cfa7c78fb24f06de9d7d7a2315
Felix Linghu <2996181745@qq.com>
2025-09-04 19:00:22 +0800

    refactor:use hashmap instead of Input

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,46 +1,20 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class WordFrequencyGame {
     public String getResult(String inputStr) {
         String[] words = inputStr.split("\\s+");
-        List<Input> inputList = new ArrayList<>();
-        for (String s : words) {
-            Input input = new Input(s, 1);
-            inputList.add(input);
+        HashMap<String,Integer> occurrenceFrequencyWord=new HashMap<>();
+        for(String word:words){
+            occurrenceFrequencyWord.put(word,occurrenceFrequencyWord.getOrDefault(word,0)+1);
         }
-        Map<String, List<Input>> map = getListMap(inputList);
-        List<Input> list = new ArrayList<>();
-        for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
-            Input input = new Input(entry.getKey(), entry.getValue().size());
-            list.add(input);
-        }
-        inputList = list;
-        inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+        List<Map.Entry<String,Integer>> sortedResult=new ArrayList<>(occurrenceFrequencyWord.entrySet());
+        Collections.sort(sortedResult, (entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
         StringJoiner joiner = new StringJoiner("\n");
-        for (Input w : inputList) {
-            String s = w.getValue() + " " + w.getWordCount();
+        for (Map.Entry sortedEntry:sortedResult) {
+            String s = sortedEntry.getKey() + " " + sortedEntry.getValue();
             joiner.add(s);
         }
         return joiner.toString();
     }
-
-private Map<String, List<Input>> getListMap(List<Input> inputList) {
-    Map<String, List<Input>> map = new HashMap<>();
-    for (Input input : inputList) {
-        if (!map.containsKey(input.getValue())) {
-            ArrayList arr = new ArrayList<>();
-            arr.add(input);
-            map.put(input.getValue(), arr);
-        } else {
-            map.get(input.getValue()).add(input);
-        }
-    }
-    return map;
-}
-
 
 }
