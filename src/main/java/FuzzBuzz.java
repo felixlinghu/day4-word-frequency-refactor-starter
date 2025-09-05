92e1c758d5e6da1be4932a4a6e62707902e83897
Felix Linghu <felix.linghu@oocl.com>
2025-09-05 11:06:31 +0800

    Test:TDD Test

--- src/main/java/FuzzBuzz.java
+++ src/main/java/FuzzBuzz.java
@@ -1,6 +1,9 @@
 public class FuzzBuzz {
 
-  public static String countOff(int order){
+  public static String countOff(int order) {
+    if (order % 3 == 0) {
+      return "Fuzz";
+    }
     return String.valueOf(order);
   }
 }
--- src/test/java/FuzzBuzzTest.java
+++ src/test/java/FuzzBuzzTest.java
@@ -6,12 +6,21 @@ import org.junit.jupiter.api.Test;
 public class FuzzBuzzTest {
 
   @Test
-  public void shoule_return_one_when_number_is_one(){
+  public void shoule_return_one_when_order_is_one(){
     //given
     int order=1;
     //when
     String result=FuzzBuzz.countOff(order);
     //then
     assertEquals("1",result);
+  }
+  @Test
+  public void shoule_return_Fuzz_when_order_is_Three(){
+    //given
+    int order=3;
+    //when
+    String result=FuzzBuzz.countOff(order);
+    //then
+    assertEquals("Fuzz",result);
   }
 }
