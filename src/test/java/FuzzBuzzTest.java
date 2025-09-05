4ae99025bc5b326d7b6162c6af1b02310c91d08a
Felix Linghu <felix.linghu@oocl.com>
2025-09-05 11:24:56 +0800

    Test:TDD Test

--- src/test/java/FuzzBuzzTest.java
+++ src/test/java/FuzzBuzzTest.java
@@ -6,40 +6,43 @@ import org.junit.jupiter.api.Test;
 public class FuzzBuzzTest {
 
   @Test
-  public void shoule_return_one_when_order_is_one(){
+  public void should_return_one_when_order_is_one() {
     //given
-    int order=1;
+    int order = 1;
     //when
-    String result=FuzzBuzz.countOff(order);
+    String result = FuzzBuzz.countOff(order);
     //then
-    assertEquals("1",result);
+    assertEquals("1", result);
   }
+
   @Test
-  public void shoule_return_Fuzz_when_order_is_Three(){
+  public void should_return_Fuzz_when_order_is_three() {
     //given
-    int order=3;
+    int order = 3;
     //when
-    String result=FuzzBuzz.countOff(order);
+    String result = FuzzBuzz.countOff(order);
     //then
-    assertEquals("Fuzz",result);
+    assertEquals("Fuzz", result);
   }
+
   @Test
-  public void shoule_return_Buzz_when_order_is_five(){
+  public void should_return_Buzz_when_order_is_five() {
     //given
-    int order=5;
+    int order = 5;
     //when
-    String result=FuzzBuzz.countOff(order);
+    String result = FuzzBuzz.countOff(order);
     //then
-    assertEquals("Buzz",result);
+    assertEquals("Buzz", result);
   }
+
   @Test
-  public void shoule_return_FuzzBuzz_when_order_is_fifteen(){
+  public void should_return_FuzzBuzz_when_order_is_fifteen() {
     //given
-    int order=15;
+    int order = 15;
     //when
-    String result=FuzzBuzz.countOff(order);
+    String result = FuzzBuzz.countOff(order);
     //then
-    assertEquals("FuzzBuzz",result);
+    assertEquals("FuzzBuzz", result);
   }
 
 }
