78878a396433b77b0c2ccbf1fa626cb1f8bdf938
Felix Linghu <felix.linghu@oocl.com>
2025-09-05 15:52:28 +0800

    Test:TDD  MarsRoverTest

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -6,6 +6,7 @@ public class MarsRover {
   private int x;
   private int y;
   private char direction;
+  public static final char[] DIRECATIONS = {'W', 'S', 'E', 'N'};
 
   public MarsRover(int x, int y, char direction) {
     this.x = x;
@@ -14,10 +15,40 @@ public class MarsRover {
   }
 
   public List<Integer> operateMarsRover(char command) {
-    List<Integer> result = new ArrayList<>();
-    result.add(x - 1);
-    result.add(y);
-    result.add((int) direction);
-    return result;
+    if (command == 'M') {
+      return marsRoverMove();
+    }
+    if (command == 'L') {
+      return marsRoverLeft();
+    }
+    return null;
+  }
+
+  private List<Integer> marsRoverLeft() {
+    ArrayList<Integer> currentLocation = new ArrayList<>();
+    currentLocation.add(x);
+    currentLocation.add(y);
+    char currentDirecton =
+        currentLocation.add()
+  }
+
+  private List<Integer> marsRoverMove() {
+    ArrayList<Integer> currentLocation = new ArrayList<>();
+    if (direction == 'N') {
+      y++;
+    }
+    if (direction == 'S') {
+      y--;
+    }
+    if (direction == 'W') {
+      x++;
+    }
+    if (direction == 'E') {
+      x--;
+    }
+    currentLocation.add(x);
+    currentLocation.add(y);
+    currentLocation.add((int) direction);
+    return currentLocation;
   }
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -22,10 +22,69 @@ class MarsRoverTest {
       List<Integer> result = new MarsRover(x, y, direction).operateMarsRover('M');
       //then
       List<Integer> expect = new ArrayList<>();
+      if (direction == 'N') {
+        y++;
+      }
+      if (direction == 'S') {
+        y--;
+      }
+      if (direction == 'W') {
+        x++;
+      }
+      if (direction == 'E') {
+        x--;
+      }
+      expect.add(x);
+      expect.add(y);
+      expect.add((int) direction);
+      assertEquals(expect, result);
+    }
+  }
+
+  @Test
+  public void should_return_direction_change_when_command_is_Left() {
+    //given
+    int x = 0;
+    int y = 0;
+    Character command = 'L';
+    List<Character> directions = new ArrayList<>();
+    directions.add('W');
+    directions.add('N');
+    directions.add('S');
+    directions.add('E');
+    for (char direction : directions) {
+      //when
+      List<Integer> result = new MarsRover(x, y, direction).operateMarsRover('L');
+      //then
+      List<Integer> expect = new ArrayList<>();
+      expect.add(0);
+      expect.add(0);
+      expect.add((int) 'S');
+      assertEquals(expect, result);
+    }
+  }
+
+  @Test
+  public void should_return_direction_change_when_command_is_Right() {
+    //given
+    int x = 0;
+    int y = 0;
+    Character command = 'M';
+    List<Character> directions = new ArrayList<>();
+    directions.add('W');
+    directions.add('N');
+    directions.add('S');
+    directions.add('E');
+    for (char direction : directions) {
+      //when
+      List<Integer> result = new MarsRover(x, y, direction).operateMarsRover('M');
+      //then
+      List<Integer> expect = new ArrayList<>();
       expect.add(-1);
       expect.add(0);
       expect.add((int) direction);
       assertEquals(expect, result);
+      fail();
     }
   }
 }
